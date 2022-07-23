32adfac0a29ecd2a1d581aaca3eef17afd841128
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-07-23 10:08:52 -0300

    Styling

---
+++ frontend/src/components/elements/navbar.svelte
@@ -0,0 +1,18 @@
+<navbar class="container"><a href="/">Início</a><a href="/">Sobre</a></navbar>
+
+<style>
+	a {
+		font-weight: bold;
+		color: var(--accent-color);
+		text-decoration: none;
+	}
+
+	.container {
+		padding: 2rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		gap: 3rem;
+		background-color: transparent;
+	}
+</style>
--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,8 +1,13 @@
 <script context="module">
+	// Components:
+	import Navbar from '../components/elements/navbar.svelte';
+
+	// Styles:
 	import '../app.css';
 </script>
 
 <svelte:head>
 	<title>3D Maker Space</title>
 </svelte:head>
+<Navbar />
 <slot />
