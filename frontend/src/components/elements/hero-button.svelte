809520e2c730517cd5139c591c87173de0b290bc
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-07-24 11:09:27 -0300

    Styling

--- frontend/src/components/elements/hero-button.svelte
+++ frontend/src/components/elements/hero-button.svelte
@@ -11,20 +11,26 @@
 		font-size: 1.2rem;
 		text-align: center;
 		color: var(--dark-color);
-		animation: glowing 10s infinite;
+		box-shadow: 0 0 3px var(--primary-color);
+		transition: all;
+		animation-duration: 1s;
+	}
+
+	button:hover {
+		animation: glowing 5s infinite;
 	}
 
 	@keyframes glowing {
 		0% {
-			filter: brightness(1.1);
+			filter: brightness(1);
 			box-shadow: 0 0 3px var(--primary-color);
 		}
 		50% {
 			filter: brightness(1.2);
-			box-shadow: 0 0 10px var(--primary-color);
+			box-shadow: 0 0 20px var(--primary-color);
 		}
 		100% {
-			filter: brightness(1.1);
+			filter: brightness(1);
 			box-shadow: 0 0 3px var(--primary-color);
 		}
 	}
